758bce12c234c975abfcdcc0b73a72f0426ad7a9
Yacinth <fevriercjjc@gmail.com>
2019-01-31 08:25:56 +0100

    Gemfile, Migrations, Models and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+p "destroy database"
+Course.destroy_all
+Student.destroy_all
+p "delete from sqlite_sequence"
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'courses'")
+ActiveRecord::Base.connection.execute("DELETE from sqlite_sequence where name = 'students'")
+
+10.times do 
+  course = Course.create!(title: Faker::Job.key_skill, description: Faker::ChuckNorris.fact)
+  p "course #{course.title}"
+end
+
+10.times do
+  student = Student.create!(course_id: rand(1..10), name: Faker::Name.first_name)
+  p "student #{student.name}"
+end
